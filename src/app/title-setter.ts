a485005bf293a1a6587df088d78d268670259582
ilam56 <ilam56@mail.missouri.edu>
2020-09-26 22:41:57 -0500

    1 file deleted

--- src/app/title-setter.ts
+++
@@ -1,9 +0,0 @@
-import { Title } from '@angular/platform-browser';
-
-export class TitleSetter {
-  public constructor(private titleService: Title) {}
-
-  public setTitle(title: string) {
-     this.titleService.setTitle(title);
-  }
-}
